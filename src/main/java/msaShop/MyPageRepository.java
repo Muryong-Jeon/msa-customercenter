e4d5813e4fd975765a68685198f9405bcb165e4a
Muryong-Jeon <muryongjeon@gmail.com>
2020-09-05 13:47:29 +0900

    first commit

---
+++ src/main/java/msaShop/MyPageRepository.java
@@ -0,0 +1,11 @@
+package msaShop;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MyPageRepository extends CrudRepository<MyPage, Long> {
+
+    List<MyPage> findByOrderId(Long orderId);
+
+}
